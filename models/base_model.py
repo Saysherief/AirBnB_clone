a91472b44da924f551f31a8aa268e6ffdb4203e3
Saysherief <suhairdama@gmail.com>
2023-07-15 12:06:56 -0700

    Solving conflict

--- models/base_model.py
+++ models/base_model.py
@@ -15,13 +15,6 @@ class BaseModel:
             *args (any): Unused.
             **kwargs (dict): key/value pairs of attributes.
         """
-<<<<<<< HEAD
-        tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid4())
-        self.created_at = datetime.today()
-        self.updated_at = datetime.today()
-=======
->>>>>>> 994e5cdb81ec42fd7aecccacf21e537925dcf56a
         if len(kwargs) != 0:
             for k, v in kwargs.items():
                 if k == "created_at" or k == "updated_at":
