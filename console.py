ef63449735d5e19c2c772bcbd84a99e1e8e73005
Honey Gt <nadhigutu17@gmail.com>
2023-07-15 12:54:31 -0700

    Command interpreter

--- console.py
+++ console.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """Entry point of the command interpreter."""
 import cmd
+import json
+import models
 
 
 class HBNBCommand(cmd.Cmd):
     """Class for HBNB command interpreter"""
 
     prompt = "(hbnb) "
+    valid_models = ["BaseModel"]
 
     def do_quit(self, arg):
         """Quit command to exit the program."""
@@ -19,6 +22,116 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing when the empty line is entered."""
         pass
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel"""
+        if arg == "":
+            print("** class name missing **")
+            return
+        try:
+            cls = arg.split()[0]
+            if cls not in HBNBCommand.valid_models:
+                print("** class doesn't exist **")
+                return
+            new_instance = models.classes[cls]()
+            new_instance.save()
+            print(new_instance.id)
+        except Exception as e:
+            print(e)
+
+    def do_show(self, arg):
+        """Prints the string representation of an instance"""
+        if arg == "":
+            print("** class name missing **")
+            return
+        try:
+            args = arg.split()
+            cls = args[0]
+            if cls not in HBNBCommand.valid_models:
+                print("** class doesn't exist **")
+                return
+            if len(args) < 2:
+                print("** instance id missing **")
+                return
+            obj_id = args[1]
+            key = "{}.{}".format(cls, obj_id)
+            if key not in models.storage.all():
+                print("** no instance found **")
+                return
+            print(models.storage.all()[key])
+        except Exception as e:
+            print(e)
+
+    def do_destroy(self, arg):
+        """Deletes an instance based on the class name and id"""
+        if arg == "":
+            print("** class name missing **")
+            return
+        try:
+            args = arg.split()
+            cls = args[0]
+            if cls not in HBNBCommand.valid_models:
+                print("** class doesn't exist **")
+                return
+            if len(args) < 2:
+                print("** instance id missing **")
+                return
+            obj_id = args[1]
+            key = "{}.{}".format(cls, obj_id)
+            if key not in models.storage.all():
+                print("** no instance found **")
+                return
+            models.storage.all().pop(key)
+            models.storage.save()
+        except Exception as e:
+            print(e)
+
+    def do_all(self, arg):
+        """Prints all string representation of all instances"""
+        try:
+            if arg == "":
+                print([str(v) for v in models.storage.all().values()])
+            elif arg in HBNBCommand.valid_models:
+                objs = [str(v) for k, v in models.storage.all().items()
+                        if arg in k]
+                print(objs)
+            else:
+                print("** class doesn't exist **")
+        except Exception as e:
+            print(e)
+
+    def do_update(self, arg):
+        """Updates an instance based on the class name and id"""
+        if arg == "":
+            print("** class name missing **")
+            return
+        try:
+            args = arg.split()
+            cls = args[0]
+            if cls not in HBNBCommand.valid_models:
+                print("** class doesn't exist **")
+                return
+            if len(args) < 2:
+                print("** instance id missing **")
+                return
+            obj_id = args[1]
+            key = "{}.{}".format(cls, obj_id)
+            if key not in models.storage.all():
+                print("** no instance found **")
+                return
+            if len(args) < 3:
+                print("** attribute name missing **")
+                return
+            if len(args) < 4:
+                print("** value missing **")
+                return
+            attr_name = args[2]
+            attr_value = args[3]
+            obj = models.storage.all()[key]
+            setattr(obj, attr_name, attr_value)
+            models.storage.save()
+        except Exception as e:
+            print(e)
 
 
 if __name__ == '__main__':
