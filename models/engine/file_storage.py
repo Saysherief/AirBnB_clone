5c2f2f3dde10304030b77428ceec694da1f81c67
Saysherief <suhairdama@gmail.com>
2023-07-16 17:55:51 -0700

    Dictionary objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,20 +17,22 @@ class FileStorage:
     """
     __file_path = "file.json"
     __objects = {}
+    __classes = {
+        "BaseModel",
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
+    }
 
     def all(self):
         """
         Returns the dictionry __objects storing all objects by class name & ID
         Overridden to include User objects
         """
-        rdict = {}
-        for k, v in FileStorage.__objects.items():
-            rdict[k] = v.to_dict()
-        # User objects are added below
-        for k, v in FileStorage.__objects.items():
-            if isinstance(v, User):
-                rdict[k] = v.to_dict()
-        return rdict
+        return FileStorage.__objects
 
     def new(self, obj):
         """
@@ -45,8 +47,8 @@ class FileStorage:
         rdict = {}
         for k, v in FileStorage.__objects.items():
             rdict[k] = v.to_dict()
-        with open(FileStorage.__file_path, mode="w", encoding="utf-8") as myfi:
-            json.dump(rdict, myfi)
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as myfile:
+            json.dump(rdict, myfile)
 
     def reload(self):
         """
@@ -54,14 +56,14 @@ class FileStorage:
         otherwise, does nothing
         """
         try:
-            with open(FileStorage.__file_path, mode="r") as myfile:
-                rdict = json.load(myfile)
-                tform = "%Y-%m-%dT%H:%M:%S.%f"
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as myf:
+                rdict = json.load(myf)
                 for k, v in rdict.items():
-                    if "User" in v["__class__"]:
-                        obj = User(**v)
-                    else:
-                        obj = BaseModel(**v)
+                    cl_name = k.split(".")[0]
+                    if cl_name not in self.__classes:
+                        continue
+                    re_cls = eval(cl_name)
+                    obj = re_cls(**v)
                     FileStorage.__objects[k] = obj
         except FileNotFoundError:
             pass
